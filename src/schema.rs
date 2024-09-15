03e28af99a46e6b587e07b89457f72679fdc3496
Egor <egor.galkin99@yandex.ru>
2024-09-15 17:03:41 +0300

    Beautify code

--- src/schema.rs
+++ src/schema.rs
@@ -349,7 +349,7 @@ async fn receive_search_request(bot: Bot, dialogue: MyDialogue, msg: Message) ->
 
 async fn receive_person_number(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
     match msg.text().map(|text| text.parse::<u8>()) {
-        Some(Ok(person_number)) if person_number >= 1 && person_number <= 6 => {
+        Some(Ok(person_number)) if (1..=6).contains(&person_number) => {
             bot.send_message(msg.chat.id, "Отправьте локацию для поиска мест")
                 .reply_markup(make_location_keyboard())
                 .await?;
